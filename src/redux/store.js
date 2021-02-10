9e023cebf992aea3ca48792854d1d4005b352a9a
ThekhoN <bennetonss@hotmail.com>
2021-02-10 22:32:13 +0530

    disable devtools explicit

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,7 @@ import rootReducer from './reducers'
 
 const store = configureStore({
   reducer: rootReducer,
-  devTools: process.env.NODE_ENV !== 'production',
+  devTools: false,
 })
 
 export default store
