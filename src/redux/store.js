8bef44ff2f47e995dbd4130df1a706dc8ae285e5
ThekhoN <bennetonss@hotmail.com>
2021-02-10 22:27:29 +0530

    add postbuild, disable devTools in prod

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,8 @@ import { configureStore } from '@reduxjs/toolkit'
 import rootReducer from './reducers'
 
 const store = configureStore({
-  reducer: rootReducer
+  reducer: rootReducer,
+  devTools: process.env.NODE_ENV !== 'production',
 })
 
 export default store
