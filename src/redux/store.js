7588871b88e73178e4f4738e43de6cfc8439d2b3
ThekhoN <bennetonss@hotmail.com>
2021-02-11 00:47:32 +0530

    add logs

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
 import rootReducer from './reducers'
 
+console.log(`process.env.NODE_ENV: ${process.env.NODE_ENV}`)
+
 const store = configureStore({
   reducer: rootReducer,
   devTools: process.env.NODE_ENV === 'development',
